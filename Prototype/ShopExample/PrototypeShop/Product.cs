7602707e445d7d802d113600bf721a888dd1c3e0
MadlenIlianova <madlenveselinova4@gmail.com>
2026-03-27 19:24:54 +0200

    example

---
+++ Prototype/ShopExample/PrototypeShop/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeShop
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int Size { get; set; }
+
+        public Product(string name, string color, decimal price, int size)
+        {
+            Name = name;
+            Color = color;
+            Price = price;
+            Size = size;
+        }
+
+        public Product Clone()
+        {
+            return new Product(Name, Color, Price, Size);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Color: {Color}, Price: {Price}, Size: {Size}");
+        }
+    }
+}
---
+++ Prototype/ShopExample/PrototypeShop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrototypeShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product template = new Product("T-Shirt", "Red", 19.99m, 42);
+
+            Product product1 = template.Clone();
+            product1.Size = 44;
+
+            Product product2 = template.Clone();
+            product2.Color = "Blue";
+
+            Product product3 = template.Clone();
+            product3.Price = 24.99m;
+
+            template.ShowInfo();
+            product1.ShowInfo();
+            product2.ShowInfo();
+            product3.ShowInfo();
+        }
+    }
+}
